b4c27effa85af5bf4b3c9fba6dc54252023478d0
macg30 <macg30@users.noreply.github.com>
2021-08-29 23:41:50 +0200

    Add UnitTest project | Example to test Product

---
+++ CameraReviews/Product/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CameraReviews.Product
+{
+	public class Product : IProduct
+	{
+		public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public string Manufacturer { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public string SKU { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public List<Feature> Feature { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+		public string GetContent()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ CameraReviewsUnitTest/CameraReview/Product/ProductTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CameraReviews.Product;
+using NSubstitute;
+
+namespace CameraReviewsUnitTest.CameraReview.Product
+{
+	[TestClass]
+	public class ProductTest
+	{
+		[TestMethod]
+		public void Product_ShouldReturnContent_Success()
+		{
+			//Setup
+			var product = Substitute.For<IProduct>(); //Mock
+			//var product = new Product(); //Implementation
+
+			//Execute
+			var content = product.GetContent();
+
+			//Assert
+			Assert.IsNotNull(content);
+			Assert.IsTrue(!string.IsNullOrEmpty(content), "Should return content but obtain null or whitespace"); //null, "", " "
+		}
+
+	}
+}
